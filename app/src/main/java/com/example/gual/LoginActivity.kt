52662bc8096fcc4b3401bf1ca7d48a9a20f4deb9
chaewon kim <kch9870@naver.com>
2021-05-24 23:23:32 +0900

    git

---
+++ app/src/main/java/com/example/gual/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.example.gual
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/gual/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gual
+
+class MainActivity {
+
+}
